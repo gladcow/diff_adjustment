3aac4f240733e9dc1f6edb683cf8d33555785515
gladcow <sergey@dash.org>
2018-01-23 18:54:08 +0300

    remove unused code

--- test_mining.py
+++ test_mining.py
@@ -103,168 +103,6 @@ def median_time_past(states):
     return sorted(times)[len(times) // 2]
 
 
-def next_bits_k(msg, mtp_window, high_barrier, target_raise_frac,
-                low_barrier, target_drop_frac, fast_blocks_pct):
-    # Calculate N-block MTP diff
-    MTP_0 = median_time_past(states[-11:])
-    MTP_N = median_time_past(states[-11-mtp_window:-mtp_window])
-    MTP_diff = MTP_0 - MTP_N
-    bits = states[-1].bits
-    target = bits_to_target(bits)
-
-    # Long term block production time stabiliser
-    t = states[-1].timestamp - states[-2017].timestamp
-    if t < TARGET_BLOCK_TIME * 2016 * fast_blocks_pct // 100:
-        msg.append("2016 block time difficulty raise")
-        target -= target // target_drop_frac
-
-    if MTP_diff > high_barrier:
-        target += target // target_raise_frac
-        msg.append("Difficulty drop {}".format(MTP_diff))
-    elif MTP_diff < low_barrier:
-        target -= target // target_drop_frac
-        msg.append("Difficulty raise {}".format(MTP_diff))
-    else:
-        msg.append("Difficulty held {}".format(MTP_diff))
-
-    return target_to_bits(target)
-
-
-def suitable_block_index(index):
-    assert index >= 3
-    indices = [index - 2, index - 1, index]
-
-    if states[indices[0]].timestamp > states[indices[2]].timestamp:
-        indices[0], indices[2] = indices[2], indices[0]
-
-    if states[indices[0]].timestamp > states[indices[1]].timestamp:
-        indices[0], indices[1] = indices[1], indices[0]
-
-    if states[indices[1]].timestamp > states[indices[2]].timestamp:
-        indices[1], indices[2] = indices[2], indices[1]
-
-    return indices[1]
-
-
-def compute_index_fast(index_last):
-    for candidate in range(index_last - 3, 0, -1):
-        index_fast = suitable_block_index(candidate)
-        if index_last - index_fast < 5:
-            continue
-        if (states[index_last].timestamp - states[index_fast].timestamp
-                >= 13 * TARGET_BLOCK_TIME):
-            return index_fast
-    raise AssertionError('should not happen')
-
-
-def compute_target(first_index, last_index):
-    work = states[last_index].chainwork - states[first_index].chainwork
-    work *= TARGET_BLOCK_TIME
-    work //= states[last_index].timestamp - states[first_index].timestamp
-    return (2 << 255) // work - 1
-
-
-def next_bits_d(msg):
-    N = len(states) - 1
-    index_last = suitable_block_index(N)
-    index_first = suitable_block_index(N - 2016)
-    interval_target = compute_target(index_first, index_last)
-    index_fast = compute_index_fast(index_last)
-    fast_target = compute_target(index_fast, index_last)
-
-    next_target = interval_target
-    if (fast_target < interval_target - (interval_target >> 2) or
-            fast_target > interval_target + (interval_target >> 2)):
-        msg.append("fast target")
-        next_target = fast_target
-    else:
-        msg.append("interval target")
-
-    prev_target = bits_to_target(states[-1].bits)
-    min_target = prev_target - (prev_target >> 3)
-    if next_target < min_target:
-        msg.append("min target")
-        return target_to_bits(min_target)
-
-    max_target = prev_target + (prev_target >> 3)
-    if next_target > max_target:
-        msg.append("max target")
-        return target_to_bits(max_target)
-
-    return target_to_bits(next_target)
-
-
-def compute_cw_target(block_count):
-    first, last = -1-block_count, -1
-    timespan = states[last].timestamp - states[first].timestamp
-    timespan = max(block_count * TARGET_BLOCK_TIME // 2, min(block_count * 2 * TARGET_BLOCK_TIME, timespan))
-    work = (states[last].chainwork - states[first].chainwork) * TARGET_BLOCK_TIME // timespan
-    return (2 << 255) // work - 1
-
-
-def next_bits_sha(msg):
-    primes = [73, 79, 83, 89, 97,
-              101, 103, 107, 109, 113, 127,
-              131, 137, 139, 149, 151]
-
-    # The timestamp % len(primes) is a proxy for previous
-    # block SHAx2 % len(primes), but that data is not available
-    # in this simulation
-    prime = primes[states[-1].timestamp % len(primes)]
-    
-    interval_target = compute_cw_target(prime)
-    return target_to_bits(interval_target)
-
-
-def next_bits_cw(msg, block_count):
-    interval_target = compute_cw_target(block_count)
-    return target_to_bits(interval_target)
-
-
-def next_bits_wt(msg, block_count, limit_precision):
-    DIFF_WEIGHT_PRECISION = 1000000
-
-    first, last = -1-block_count, -1
-    last_target = bits_to_target(states[last].bits)
-    last_target_fixed = last_target // DIFF_WEIGHT_PRECISION
-    timespan = 0
-    prior_timestamp = states[first].timestamp
-    for i in range(first + 1, last + 1):
-        target_i = bits_to_target(states[i].bits)
-        # Prevent negative time_i values
-        timestamp = max(states[i].timestamp, prior_timestamp)
-        time_i = timestamp - prior_timestamp
-        prior_timestamp = timestamp
-        if limit_precision:
-            adj_time_i = time_i * (target_i // DIFF_WEIGHT_PRECISION) // last_target_fixed
-        else:
-            adj_time_i = time_i * target_i // last_target  # Difficulty weight
-        timespan += adj_time_i * (i - first)  # Recency weight
-    timespan = timespan * 2 // (block_count + 1)  # Normalize recency weight
-    target = last_target * timespan  # Standard retarget
-    target //= TARGET_BLOCK_TIME * block_count
-    return target_to_bits(target)
-
-
-def next_bits_wt_compare(msg, block_count, limit_precision):
-    with open("current_state.csv", 'w') as fh:
-        for s in states:
-            fh.write("%s,%s,%s\n" % (s.height, s.bits, s.timestamp))
-
-    from subprocess import Popen, PIPE
-
-    process = Popen(["./cashwork"], stdout=PIPE)
-    (next_bits, err) = process.communicate()
-    exit_code = process.wait()
-
-    next_bits = int(next_bits.decode())
-    next_bits_py = next_bits_wt(msg, block_count, limit_precision)
-    if next_bits != next_bits_py:
-        print("ERROR: Bits don't match. External %s, local %s" % (next_bits, next_bits_py))
-        assert(next_bits == next_bits_py)
-    return next_bits
-
-
 def next_bits_dgw3(msg, block_count):
     ''' Dark Gravity Wave v3 from Dash '''
     block_reading = -1  # dito
@@ -531,20 +369,6 @@ def next_bits_proportional_error(msg, window):
     return target_to_bits(result_target)
 
 
-def next_bits_m2(msg, window_1, window_2):
-    interval_target = compute_target(-1 - window_1, -1)
-    interval_target += compute_target(-2 - window_2, -2)
-    return target_to_bits(interval_target >> 1)
-
-
-def next_bits_m4(msg, window_1, window_2, window_3, window_4):
-    interval_target = compute_target(-1 - window_1, -1)
-    interval_target += compute_target(-2 - window_2, -2)
-    interval_target += compute_target(-3 - window_3, -3)
-    interval_target += compute_target(-4 - window_4, -4)
-    return target_to_bits(interval_target >> 2)
-
-
 def block_time(mean_time):
     # Sample the exponential distn
     sample = random.random()
@@ -583,40 +407,6 @@ def next_step(algo, scenario):
 Algo = namedtuple('Algo', 'next_bits params')
 
 Algos = {
-    'k-1': Algo(next_bits_k, {
-        'mtp_window': 6,
-        'high_barrier': 60 * 128,
-        'target_raise_frac': 64,   # Reduce difficulty ~ 1.6%
-        'low_barrier': 60 * 30,
-        'target_drop_frac': 256,   # Raise difficulty ~ 0.4%
-        'fast_blocks_pct': 95,
-    }),
-    'k-2': Algo(next_bits_k, {
-        'mtp_window': 4,
-        'high_barrier': 60 * 55,
-        'target_raise_frac': 100,   # Reduce difficulty ~ 1.0%
-        'low_barrier': 60 * 36,
-        'target_drop_frac': 256,   # Raise difficulty ~ 0.4%
-        'fast_blocks_pct': 95,
-    }),
-    'd-1': Algo(next_bits_d, {}),
-    'cw-72': Algo(next_bits_cw, {
-        'block_count': 72,
-    }),
-    'cw-108': Algo(next_bits_cw, {
-        'block_count': 108,
-    }),
-    'cw-144': Algo(next_bits_cw, {
-        'block_count': 144,
-    }),
-    'cw-sha-16': Algo(next_bits_sha, {}),
-    'cw-180': Algo(next_bits_cw, {
-        'block_count': 180,
-    }),
-    'wt-144': Algo(next_bits_wt, {
-        'block_count': 144,
-        'limit_precision': False
-    }),
     'dgw3-24': Algo(next_bits_dgw3, {  # 24-blocks, like Dash
         'block_count': 24,
     }),
@@ -646,21 +436,6 @@ Algos = {
     }),
     'pe': Algo(next_bits_proportional_error, {
         'window': 10
-    }),
-    'meng-1': Algo(next_bits_m2, {  # mengerian_algo_1
-        'window_1': 71,
-        'window_2': 137,
-    }),
-    'meng-2': Algo(next_bits_m4, {  # mengerian_algo_2
-        'window_1': 13,
-        'window_2': 37,
-        'window_3': 71,
-        'window_4': 137,
-    }),
-    # runs wt-144 in external program, compares with python implementation.
-    'wt-144-compare': Algo(next_bits_wt_compare, {
-        'block_count': 144,
-        'limit_precision': True
     })
 }
 
